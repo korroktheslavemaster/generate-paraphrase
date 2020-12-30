35c3619d00507daa1e2b5b0dc3593129d951db9b
Arpit Tarang Saxena <arpit.tarang@gmail.com>
2020-12-31 00:10:05 +0530

    basic working

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    id: undefined,
+    q: "",
+    value: ""
+  }
+  componentDidMount() {
+    fetch('/api/randomOriginal').then(res => res.json()).then(({id, text: q}) => this.setState({id, q}))
+  }
+  onChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ original_id: this.state.id, text: this.state.value})
+    };
+    fetch('/api/paraphrase', requestOptions)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      // .then(data => this.setState({ postId: data.id }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.q || "..."}</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.value} onChange={this.onChange}></input>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,8 +1,50 @@
+require('dotenv').config()
 const express = require("express");
+var bodyParser = require('body-parser')
+const { Client } = require('pg')
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 const path = require("path");
 app.use(express.static("public"));
 app.set("port", process.env.PORT || 3001);
+
+
+/// pg stuff
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+});
+client.connect();
+pgInit = async () => {
+    await client.query('CREATE TABLE if not exists originals (id SERIAL PRIMARY KEY, text VARCHAR(500) NOT NULL);')
+    await client.query('CREATE TABLE if not exists paraphrases (id SERIAL PRIMARY KEY, text VARCHAR(500) NOT NULL, original_id integer references originals(id));')
+}
+pgInit()
+///
+
+app.get('/api/randomOriginal', (req, res) => {
+    client.query("SELECT * from originals order by random() limit 1;", (err, res2) => {
+        if (err) {
+            res.send({error: err});
+        } else {
+            if (res2.rowCount == 1) {
+                res.send(res2.rows[0]);
+            } else {
+                res.send({error: "no rows"});
+            }
+        }
+    })
+})
+
+app.post('/api/paraphrase', (req, res) => {
+    const {original_id, text} = req.body;
+    client.query(`INSERT INTO paraphrases (text, original_id) values ('${text}', ${original_id});`)
+        .then(res2 => res.send(res2.rows))
+        .catch(err => res.send({error: err}))
+})
 
 // after api and auth routes so that doesn't mess with them?
 if (process.env.NODE_ENV == "production") {
@@ -12,11 +54,8 @@ if (process.env.NODE_ENV == "production") {
     app.get("/*", function(req, res) {
       res.sendFile(path.join(__dirname, "client/build", "index.html"));
     });
-  }
-  
-  app.listen(app.get("port"), () => {
+}
+
+app.listen(app.get("port"), () => {
     console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
-  });
-  
-  // for use in testing
-  module.exports = app;
+});
